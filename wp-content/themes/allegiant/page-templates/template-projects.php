ac7646f7b3345ea255d0b6eb89a3a438000219fa
brendanmoorep <brendan.moore-penaskovic@libertymutual.com>
2017-06-12 11:22:01 -0700

    adding filtering and categories to projects page. No javascript yet

--- wp-content/themes/allegiant/page-templates/template-projects.php
+++ wp-content/themes/allegiant/page-templates/template-projects.php
@@ -15,11 +15,38 @@
 				</div>
 			</div>
 			<?php endwhile; ?>
+            <div id="taxonomies-filter">
+                <div class="section">
+                    <span><b>Filter by:</b></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Project Type <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">All Projects</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Current Projects</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Old Projects</a></li>
+                        </ul>
+                    </div>
+                </div>
 
-			<?php
-                cpotheme_secondary_menu('property_type', 'menu-portfolio');
-            ?>
-
+                <div class="section categories-list">
+                    <span><b>Categories:</b></span>
+                    <?php
+                    // cpotheme_secondary_menu('property_type', 'menu-portfolio');
+                    $propertyTypes = get_terms('property_type', 'order=ASC&orderby=name');
+                    if(sizeof($propertyTypes) > 0){
+                        foreach($propertyTypes as $pt):
+                            if($pt->slug !== "new" && $pt->slug !== "archived"){
+                                echo '<button id="taxonomy-filter" data-tax-type="' . $pt->slug . '" type="button" class="btn btn-default ' . $pt->slug . '">' . $pt->name . '</button>';
+                            }
+                        endforeach;
+                    }
+                    ?>
+                </div>
+            </div>
 			<?php if(get_query_var('paged')) $current_page = get_query_var('paged'); else $current_page = 1; ?>	
 			<?php $query = new WP_Query('post_type=projects&paged='.$current_page.'&posts_per_page=16&order=ASC&orderby=menu_order'); ?>
             <?php
